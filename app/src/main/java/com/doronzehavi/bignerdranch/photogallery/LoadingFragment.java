33add857509be3d8fe726e11569635c637a5a503
Doron <devdoron@gmail.com>
2015-09-09 00:26:22 -0700

    Initial commit

---
+++ app/src/main/java/com/doronzehavi/bignerdranch/photogallery/LoadingFragment.java
@@ -0,0 +1,24 @@
+package com.doronzehavi.bignerdranch.photogallery;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by D on 9/7/2015.
+ */
+public class LoadingFragment  extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/doronzehavi/bignerdranch/photogallery/QueryPreferences.java
@@ -0,0 +1,34 @@
+package com.doronzehavi.bignerdranch.photogallery;
+
+
+import android.content.Context;
+import android.preference.PreferenceManager;
+
+public class QueryPreferences {
+    private static final String PREF_SEARCH_QUERY = "searchQuery";
+    private static final String PREF_LAST_RESULT_ID = "lastResultId";
+
+    public static String getStoredQuery(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getString(PREF_SEARCH_QUERY, null);
+    }
+
+    public static void setStoredQuery(Context context, String query) {
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putString(PREF_SEARCH_QUERY, query)
+                .apply();
+    }
+
+    public static String getLastResultId(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getString(PREF_LAST_RESULT_ID, null);
+    }
+
+    public static void setLastresultId(Context context, String lastresultId) {
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putString(PREF_LAST_RESULT_ID, lastresultId)
+                .apply();
+    }
+}
